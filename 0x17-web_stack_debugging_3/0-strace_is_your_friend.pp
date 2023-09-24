334fb7d0318633e14afa1b194be43d529e42ab98
JojoBaPb <sipo.jmudenda@gmail.com>
2023-09-24 09:46:50 +0200

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fix for bad 'phpp' extensions to 'php' in WordPress file 'wp-settings.php'
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
